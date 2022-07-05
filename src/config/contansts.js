2366bb80e96468f72c438842d237f7759c905655
hyooa <hyoyung3231@gmail.com>
2022-07-05 11:25:52 +0900

    배포 준비 완료

--- src/config/contansts.js
+++ src/config/contansts.js
@@ -1,1 +1,4 @@
-export const API_URL = "http://localhost:3000";
+// export const API_URL = "http://localhost:3000";
+// export const API_URL = process.env.NODE_ENV ===
+// 'production' ? "" : 'localhost' ;
+export const API_URL = "https://hyooa.herokuapp.com";
